f8d337e5c880f56584da2e470b98d8938535ac35
Tiago Henriques <91702960+0gait@users.noreply.github.com>
2023-06-08 22:40:25 +0100

    updt

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var typed = new Typed(".typing", {
-    strings: ["a Computer Science Engineer", "an Athlete", "s"],
+    strings: ["a Computer Science Engineer", "an Athlete"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
