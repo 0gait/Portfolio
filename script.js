22bfbfcee05522fc5bd84d0b2a376c6514a4fcc5
Tiago Henriques <91702960+0gait@users.noreply.github.com>
2023-06-16 17:01:21 +0100

    updt

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  var typed = new Typed(".typing", {
-    strings: ["a Computer Science Engineer", "an Athlete"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-});
